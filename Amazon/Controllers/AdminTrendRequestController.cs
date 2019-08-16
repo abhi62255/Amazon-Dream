4b09035d6d0f4bd31948572a7c82986755f5c60c
Abhishek Sharma <asharma1@xpanxion.com>
2019-08-16 18:22:37 +0530

    Signed-off-by: Abhishek Sharma <asharma1@xpanxion.com> Trend

---
+++ Amazon/Controllers/AdminTrendRequestController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Amazon;
+using Amazon.Models;
+
+namespace Amazon.Controllers
+{
+    public class AdminTrendRequestController : Controller
+    {
+        private AKARTDBContext db = new AKARTDBContext();
+
+        // GET: AdminTrendRequest
+        public ActionResult Index()
+        {
+            var trendRequest = db.TrendRequest.Include(t => t.Product);
+            return View(trendRequest.ToList());
+        }
+
+
+        // GET: AdminTrendRequest/Delete/5
+        public ActionResult Accept(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TrendRequest trendRequest = db.TrendRequest.Find(id);
+            if (trendRequest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trendRequest);
+        }
+
+        // POST: AdminTrendRequest/Delete/5
+        [HttpPost, ActionName("Accept")]
+        [ValidateAntiForgeryToken]
+        public ActionResult AcceptConfirmed(int id)
+        {
+            TrendRequest trendRequest = db.TrendRequest.Find(id);
+            var modelT = new Trend();
+            modelT.Product_ID = trendRequest.Product_ID;
+            db.Trend.Add(modelT);
+            db.TrendRequest.Remove(trendRequest);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // GET: AdminTrendRequest/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TrendRequest trendRequest = db.TrendRequest.Find(id);
+            if (trendRequest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trendRequest);
+        }
+
+        // POST: AdminTrendRequest/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TrendRequest trendRequest = db.TrendRequest.Find(id);
+            db.TrendRequest.Remove(trendRequest);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Amazon/Controllers/SellerTrendRequestController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Amazon;
+using Amazon.Models;
+
+namespace Amazon.Controllers
+{
+    [Authorize(Users = "SELLER")]
+    public class SellerTrendRequestController : Controller
+    {
+        private AKARTDBContext db = new AKARTDBContext();
+
+        // GET: SellerTrendRequestS
+        public ActionResult Index()
+        {
+            var id = Convert.ToInt32(Session["SellerID"]);
+            var modelP = db.Product.Where(p => p.ID == id).ToList();
+            var modelTR = db.TrendRequest.Select(p => p.Product_ID).ToList();//need refactor
+
+            var trendRequest = db.TrendRequest.Include(t => t.Product);
+
+            return View(trendRequest.ToList());
+        }
+
+      
+
+        // GET: SellerTrendRequest/Create
+        public ActionResult Create()
+        {
+            var id = Convert.ToInt32(Session["SellerID"]);
+            var product = db.Product.Where(p=>p.Seller_ID == id).ToList();
+            var modelT = db.Trend.Select(t => t.Product_ID).ToList();
+            var modelTR = db.TrendRequest.Select(t => t.Product_ID).ToList();
+            var modelPR = db.ProductRequest.Select(t => t.Product_ID).ToList();
+
+            foreach (var i in modelT)
+            {
+                var pro = db.Product.Where(p => p.ID == i).FirstOrDefault();
+                product.Remove(pro);
+            }
+            foreach (var i in modelTR)
+            {
+                var pro = db.Product.Where(p => p.ID == i).FirstOrDefault();
+                product.Remove(pro);
+            }
+            foreach (var i in modelPR)
+            {
+                var pro = db.Product.Where(p => p.ID == i).FirstOrDefault();
+                product.Remove(pro);
+            }
+            ViewBag.Product_ID = new SelectList(product, "ID", "ProductName");
+            return View();
+        }
+
+        // POST: SellerTrendRequest/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Product_ID")] TrendRequest trendRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TrendRequest.Add(trendRequest);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Product_ID = new SelectList(db.Product, "ID", "ProductName", trendRequest.Product_ID);
+            return View(trendRequest);
+        }
+
+      
+
+        // GET: SellerTrendRequest/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TrendRequest trendRequest = db.TrendRequest.Find(id);
+            if (trendRequest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trendRequest);
+        }
+
+        // POST: SellerTrendRequest/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TrendRequest trendRequest = db.TrendRequest.Find(id);
+            db.TrendRequest.Remove(trendRequest);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
